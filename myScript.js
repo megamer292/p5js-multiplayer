cae78a40ad6670f450ae92c621239e51792bfea4
megamer292 <136795907+megamer292@users.noreply.github.com>
2026-04-16 09:40:49 -0700

    Add initial socket connection and drawing logic

---
+++ myScript.js
@@ -0,0 +1,37 @@
+var px = 0;
+var py = 0;
+var list = [];
+const socket = io("http://172.22.244.100:2077/");
+
+socket.on('connection', () => {
+	console.log("connected");
+});
+
+socket.on("positions", listy=>{
+	list = listy;
+})
+
+function setup() {
+
+	createCanvas(windowWidth, windowHeight);
+	background(100);
+}
+
+function draw() {
+	background(100);
+	if (keyIsDown(LEFT_ARROW)) {
+		px -= 1;
+	}
+	if (keyIsDown(RIGHT_ARROW)) {
+		px += 1;
+	}
+	if (keyIsDown(UP_ARROW)) {
+		py -= 1;
+	}
+	if (keyIsDown(DOWN_ARROW)) {
+		py += 1;
+	}
+	for (let i=0; i<list.length;i++) {
+		circle(list[i][0], list[i][1], 10);
+	}
+}
